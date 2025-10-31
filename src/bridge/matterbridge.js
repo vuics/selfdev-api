bae370cf908778300f104b3bb985932a1f680c8f
Artem Arakcheev <artarakcheev@gmail.com>
2025-10-31 20:59:56 +0300

    Spawn Matterbridge process from its bridge

--- src/bridge/matterbridge.js
+++ src/bridge/matterbridge.js
@@ -1,8 +1,8 @@
+import { spawn } from 'child_process'
+
 import { log, warn, error, Verbose } from '../services.js'
 import Connector from './connector.js'
-// import '../mongo.js'
 import conf from '../conf.js'
-// import { extractAndParseJson } from '../utils/helper.js'
 
 const verbose = Verbose('sd:bridge/matterbridge'); verbose('')
 
@@ -10,17 +10,48 @@ export default class Matterbridge extends Connector {
   constructor (args) {
     super(args)
     // const { bridge } = args
-    verbose('Connector constructed')
+    verbose('Matterbridge constructed')
   }
 
   async start () {
     super.start()
-    verbose('Connector started')
+    verbose('Matterbridge started')
+    try {
+
+    } catch (err) {
+      error('Error starting Matterbridge:', err)
+    }
+
+    const command = '/bin/matterbridge';
+    const args = ['-conf', '/etc/matterbridge/matterbridge.toml'];
+
+    this.matterbridge = spawn(command, args, { stdio: 'inherit' });
+
+    // Handle errors
+    this.matterbridge.on('error', (err) => {
+      console.error('Failed to start matterbridge:', err);
+    });
+
+    // Handle exit
+    this.matterbridge.on('exit', (code, signal) => {
+      if (code !== null) {
+        console.log(`Matterbridge exited with code ${code}`);
+      } else {
+        console.log(`Matterbridge was killed by signal ${signal}`);
+      }
+    });
   }
 
   async stop () {
     super.stop()
-    verbose('Connector stopped')
+    verbose('Matterbridge stopped')
+
+    if (!this.matterbridge.killed) {
+      this.matterbridge.kill('SIGTERM'); // or 'SIGINT' depending on graceful shutdown
+      console.log('Sent SIGTERM to Matterbridge');
+    } else {
+      console.log('Matterbridge is already stopped');
+    }
   }
 }
 
